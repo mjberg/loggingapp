c3ed7a5ec5205d2696e54dafa7a5e28672a48ac7
Matt Berg <matt@puresomni.com>
2018-08-06 18:02:26 -0400

    more updates to diary

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,35 @@
 class EntriesController < ApplicationController
-  def index
-    @entries = Entry.all
+    def index
+      @entries = Entry.all
+      @websites = [
+        ["http://railsgirls.com", "Rails Guys"],
+        ["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
+        ["http://guides.rubyonrails.org", "Ruby on Rails Guides"],
+      ]
+    end
+
+    def show
+      @entry = Entry.find(params["id"])
+    end
+
+    def new
+    end
+
+      def create
+        entry_params = params["entry"].permit("title", "contents")
+      entry = Entry.create(entry_params)
+      redirect_to(entry_path(entry))
+    end
+
+    def edit
+    @entry = Entry.find(params["id"])
   end
-  
-  def show
-  end
+
+
+def update
+  entry_params = params["entry"].permit("title", "contents")
+  entry = Entry.find(params["id"])
+  entry.update(entry_params)
+  redirect_to(entry_path(entry))
+end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @entries = Entry.all 
+    @entries = Entry.all
     @websites = [
-      ["http://railsgirls.com", "Rails Girls"],
+      ["http://railsgirls.com", "Rails Guys"],
       ["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
       ["http://guides.rubyonrails.org", "Ruby on Rails Guides"],
     ]
